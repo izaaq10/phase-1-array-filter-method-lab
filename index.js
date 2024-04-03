a0b3dab7aa3aca096163ae9791d24bc088a4a3e4
Izaaq <odongoizaaq@gmail.com>
2024-04-03 04:25:26 -0400

    Adds the filter-lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching (drivers, Bobby){
+    const filteredDrivers = drivers.filter((d) => d.toLowerCase() === Bobby.toLowerCase()
+)
+return filteredDrivers
+    
+}
+
+function fuzzyMatch(drivers, Bobby) {
+    const matchedDrivers = drivers.filter(
+      (d) => d.slice(0, 2) === Bobby.slice(0, 2)
+    );
+    return matchedDrivers;
+  }
+    function matchName(drivers, name) {
+      const matchedDrivers = drivers.filter((driver) => driver.name === name);
+      return matchedDrivers;
+    }
